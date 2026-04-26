8fd28192bcfb5468ebc13f1e90954a6741013941
Yoshikazu Nojima <mail@ynojima.net>
2026-04-26 19:39:08 +0900

    Also handle missing PO file in DocumentProcessor
    DocumentProcessor.translate has the same nil issue when
    po_repository.load_file returns nil. Add nil check and a test
    for the missing PO file case.

--- lib/jekyll/l10n/document_processor.rb
+++ lib/jekyll/l10n/document_processor.rb
@@ -22,6 +22,7 @@ module Jekyll
           document_path = @jekyll_document.relative_path
           po_file_path = Jekyll::L10n::Util.resolve_po_path(document_path, @jekyll_l10n_config.po_base_dir)
           po = @po_repository.load_file(po_file_path.to_path)
+          return if po.nil?
           accept_mt = @jekyll_l10n_config.accept_mt
           @jekyll_document.data['title'] = resolve_translation(po, @jekyll_document.data['title'], accept_mt)
           @jekyll_document.data['synopsis'] = resolve_translation(po, @jekyll_document.data['synopsis'], accept_mt)
